2e7f7a59c44c5dd60e40efdba2a6c459c665e673
jaminadey <jaminadey@gmail.com>
2021-07-15 12:10:28 +1200

    adding div - testing repo

--- applications/payments/src/CreditCardForm.jsx
+++ applications/payments/src/CreditCardForm.jsx
@@ -5,18 +5,20 @@ const CreditCardForm = (props) => {
 
     return (
         <>
-            <h3>Card Payment</h3>
-            <p>Enter you credit or debit card details below (Visa or Mastercard). No transaction fees apply.</p>
-            <label>Card number</label>
-            <FormControl type="text" />
-            <div style={{ marginTop: 10 }}>
-                <div style={{ width: 240, float: "left" }}>
-                    <label>Expiry date</label>
-                    <FormControl type="text" placeholder="MM/YY" />
-                </div>
-                <div style={{ width: 240, float: "left", marginLeft: 20 }}>
-                    <label>CVC</label>
-                    <FormControl type="text" />
+            <div>
+                <h3>Card Payment</h3>
+                <p>Enter you credit or debit card details below (Visa or Mastercard). No transaction fees apply.</p>
+                <label>Card number</label>
+                <FormControl type="text" />
+                <div style={{ marginTop: 10 }}>
+                    <div style={{ width: 240, float: "left" }}>
+                        <label>Expiry date</label>
+                        <FormControl type="text" placeholder="MM/YY" />
+                    </div>
+                    <div style={{ width: 240, float: "left", marginLeft: 20 }}>
+                        <label>CVC</label>
+                        <FormControl type="text" />
+                    </div>
                 </div>
             </div>
         </>
